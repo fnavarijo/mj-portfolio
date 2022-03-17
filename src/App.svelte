fd036ec2aeb45b678e18397b0baf623c680fe449
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-03-17 01:36:02 -0600

    Adding products section

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,14 @@
 <script>
   import AppSection from './lib/AppSection.svelte';
   import HomeSection from './lib/sections/HomeSection.svelte';
+  import ProductsSection from './lib/sections/ProductsSection.svelte';
   import AppSectionScroller from './lib/AppSectionScroller.svelte';
 </script>
 
 <main>
   <AppSectionScroller>
     <HomeSection />
-    <AppSection id="next-section" --background-color="rgb(19, 141, 131)" />
+    <ProductsSection />
     <AppSection id="last-section" />
   </AppSectionScroller>
 </main>
---
+++ src/lib/sections/ProductsSection.svelte
@@ -0,0 +1,40 @@
+<script>
+  import AppSection from '../AppSection.svelte';
+</script>
+
+<AppSection id="next-section">
+  <div class="products-section">
+    <div class="products-section__product product">
+      <span class="product__number">01.</span>
+      <h2 class="product__title">Tulio</h2>
+      <button>Visitar</button>
+    </div>
+  </div>
+</AppSection>
+
+<style>
+.products-section {
+  width: 100%;
+  height: 100%;
+  background-color: #a33024;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-areas:  "a b"
+                        "c d";
+}
+
+.products-section__product {
+  grid-area: c;
+  align-self: flex-end;
+  margin: 0 0 4rem 4rem;
+}
+
+.product__number {
+  font-size: 4rem;
+}
+
+.product__title {
+  font-size: 8rem;
+}
+</style>
