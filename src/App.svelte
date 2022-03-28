d422bad817370197a073115f8f42e789db233cf7
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-03-28 12:45:28 -0600

    Disabling scroller

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,12 @@
   import AppSection from './lib/AppSection.svelte';
   import HomeSection from './lib/sections/HomeSection.svelte';
   import ProductsSection from './lib/sections/ProductsSection.svelte';
-  import AppSectionScroller from './lib/AppSectionScroller.svelte';
 </script>
 
 <main>
-  <AppSectionScroller>
-    <HomeSection />
-    <ProductsSection />
-    <AppSection id="last-section" />
-  </AppSectionScroller>
+  <HomeSection />
+  <ProductsSection />
+  <AppSection id="last-section" />
 </main>
 
 <style>
@@ -18,7 +15,4 @@
   font-family: 'Montserrat', sans-serif;
 }
 
-:global(body) {
-  overflow-y: hidden;
-}
 </style>
--- src/lib/AppSection.svelte
+++ src/lib/AppSection.svelte
@@ -13,7 +13,7 @@
 .app-article {
   --base-background-color: #fff;
   
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   display: flex;
   justify-content: center;
