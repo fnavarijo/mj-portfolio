9679194b41862b82ed4ee817eeed23c0948eaad9
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-03-29 01:01:10 -0600

    feat: Adding carousel

---
+++ src/lib/components/carousel/CarouselContainer.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Carousel from 'svelte-carousel';
+  import CarouselSlide from './CarouselSlide.svelte';  
+</script>
+
+<Carousel arrows={false} dots={false} autoplay>
+  <CarouselSlide title="Tulio" backgroundImage="https://res.cloudinary.com/dkvtpo8w1/image/upload/v1648501762/MJ%20Portafolio/siednji-leon-XuwE8fbyArs-unsplash.jpg" />
+  <CarouselSlide title="Tulio" backgroundImage="https://res.cloudinary.com/dkvtpo8w1/image/upload/v1648497608/MJ%20Portafolio/sumaid-pal-singh-bakshi-EKoGseH8NKs-unsplash.jpg" />
+  <CarouselSlide title="Tulio" backgroundImage="https://res.cloudinary.com/dkvtpo8w1/image/upload/v1648501785/MJ%20Portafolio/simon-lee-egWTpKFu8rU-unsplash.jpg" />
+</Carousel>
---
+++ src/lib/components/carousel/CarouselSlide.svelte
@@ -0,0 +1,47 @@
+<script>
+  let title;
+  let backgroundImage = 'https://res.cloudinary.com/dkvtpo8w1/image/upload/v1648497608/MJ%20Portafolio/sumaid-pal-singh-bakshi-EKoGseH8NKs-unsplash.jpg';
+
+  export { title, backgroundImage };
+</script>
+
+<section class="carousel-slide" style={`--background-image: url('${backgroundImage}')`}>
+  <header class="carousel-slide__header-container">
+    <h2 class="carousel-slide__header">
+      {title}
+    </h2>
+  </header>
+</section>
+
+<style>
+  .carousel-slide {
+    --default-image: url('https://res.cloudinary.com/dkvtpo8w1/image/upload/v1648497608/MJ%20Portafolio/sumaid-pal-singh-bakshi-EKoGseH8NKs-unsplash.jpg');
+
+    height: 100vh;
+    width: 100%;
+    background: var(--background-image, var(--default-image));
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+    position: relative;
+
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    grid-template-areas:  "a b"
+                          "c d";
+  }
+
+  .carousel-slide__header-container {
+    position: absolute;
+    grid-area: c;
+    align-self: flex-end;
+    color: #fff;
+    margin: 0 0 2rem 5rem;
+  }
+
+  .carousel-slide__header {
+    font-size: 6rem;
+
+  }
+</style>
