4b8b33ff4788805486739b38c03c7cc4bf45ee6f
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-03-30 00:26:07 -0600

    fix: Adding support for producto route

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+import { resolve, dirname } from 'path';
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+const _dirname = typeof __dirname !== 'undefined'
+  ? __dirname
+  : dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(_dirname, 'index.html'),
+        producto: resolve(_dirname, 'producto/index.html')
+      }
+    }
+  },
   optimizeDeps: {
     include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
   }
