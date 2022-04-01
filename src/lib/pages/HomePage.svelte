d54825dda228c8db746f8f1c2491e9529eb8a574
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-04-01 00:26:31 -0600

    Adding quote

--- src/lib/pages/HomePage.svelte
+++ src/lib/pages/HomePage.svelte
@@ -3,11 +3,13 @@
   import HomeSection from '../sections/HomeSection.svelte';
   import PortfolioSection from '../sections/PortfolioSection.svelte';
   import ProductsSection from '../sections/ProductsSection.svelte';
+  import QuoteSection from '../sections/QuoteSection.svelte';
 </script>
 
 <div>
   <HomeSection />
   <ProductsSection />
   <PortfolioSection />
+  <QuoteSection />
   <ContactSection />
 </div>
---
+++ src/lib/sections/QuoteSection.svelte
@@ -0,0 +1,34 @@
+<script>
+  import AppSection from '../AppSection.svelte';
+</script>
+
+<AppSection class="quote-section">
+  <div class="quote-section__container">
+    <p class="quote-section__quote">"Failing doesn't mean you are a failure"</p>
+    <span class="quote-section__author">- Brad Henry</span>
+  </div>
+</AppSection>
+
+<style>
+  :global(.quote-section) {
+    min-height: 30vh !important;
+    /* background-color: #F0F0DB !important; */
+  }
+
+  .quote-section__container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .quote-section__quote {
+    font-size: 2rem;
+    font-weight: bold;
+  }
+
+  .quote-section__author {
+    font-size: 1.5;
+    align-self: flex-end;
+  }
+</style>
